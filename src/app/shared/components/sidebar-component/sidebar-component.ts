0c8bc43e10cf745a10cb96458cf8b1188092b42a
MaryAnzh <maryang@mail.ru>
2026-05-15 15:11:21 +0300

    feat: add sidebar in layout

--- src/app/shared/components/sidebar-component/sidebar-component.ts
+++ src/app/shared/components/sidebar-component/sidebar-component.ts
@@ -4,16 +4,19 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SIDEBAR_ITEMS } from '../../constants';
 import type { ISidebarItem } from '../../../models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-component',
   imports: [
+    CommonModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule
   ],
   templateUrl: './sidebar-component.html',
   styleUrl: './sidebar-component.scss',
+  standalone: true,
 })
 export class SidebarComponent {
   menu: ISidebarItem[] = SIDEBAR_ITEMS;
