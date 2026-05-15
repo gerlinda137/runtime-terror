eeab1e45c03b57a235299c8d65a6e05831b60463
MaryAnzh <maryang@mail.ru>
2026-05-15 20:05:05 +0300

    feat: add sidebar mob version

--- src/app/shared/components/sidebar-component/sidebar-component.ts
+++ src/app/shared/components/sidebar-component/sidebar-component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, effect, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { SIDEBAR_ITEMS } from '../../constants';
 import type { ISidebarItem } from '../../../models';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
 import { Typography } from "../typography/typography";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar-component',
@@ -23,9 +24,31 @@ import { Typography } from "../typography/typography";
   standalone: true,
 })
 export class SidebarComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
   menu: ISidebarItem[] = SIDEBAR_ITEMS;
   isMobile = false;
   openedItem: string | null = null;
+
+  constructor(private bp: BreakpointObserver) { }
+
+  ngOnInit() {
+    this.bp.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+
+        if (this.isMobile) {
+          this.sidenav.close();
+        } else {
+          this.sidenav.open();
+        }
+      });
+  }
+
+  closeBurger() {
+    if (!this.isMobile) { return }
+    this.sidenav.close();
+  }
 
   toggle(label: string) {
     this.openedItem = this.openedItem === label ? null : label;
