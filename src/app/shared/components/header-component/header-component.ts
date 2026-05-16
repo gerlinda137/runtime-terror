078ab7583d16b986c6c99053f837ae101a220430
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-16 22:43:37 +0400

    feaе: add header styles

--- src/app/shared/components/header-component/header-component.ts
+++ src/app/shared/components/header-component/header-component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Typography } from "../typography/typography";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [Typography,MatFormFieldModule,MatInputModule,MatIconModule],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss',
 })
