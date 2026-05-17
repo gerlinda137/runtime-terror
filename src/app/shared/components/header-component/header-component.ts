7317c05f63c6781da1c77ecc19907ba73e54c26a
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-05-17 15:58:06 +0400

    feat: enhance header component with user actions and search functionality

--- src/app/shared/components/header-component/header-component.ts
+++ src/app/shared/components/header-component/header-component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Typography } from "../typography/typography";
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import {MatBadgeModule} from '@angular/material/badge';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header-component',
-  imports: [Typography,MatFormFieldModule,MatInputModule,MatIconModule],
+  imports: [Typography,MatBadgeModule,MatMenuModule,MatIconModule],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isLogedin = true;
+}
