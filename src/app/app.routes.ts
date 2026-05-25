db4c482d0bb78bbafd443577a82f406004574577
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-05-25 19:34:37 +0400

    refactor: update Dashboard component imports and file references

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './features/dashboard/dashboard';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: Dashboard },
+  { path: 'dashboard', component: DashboardComponent },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { TrendingMarket } from '../../shared/components/trending-market/trending-market';
+import { TrendingMarket } from '../shared/components/trending-market/trending-market';
 
 @Component({
   selector: 'app-dashboard',
   imports: [TrendingMarket],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
 })
-export class Dashboard {}
+export class DashboardComponent {}
