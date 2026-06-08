24623bc8eb5849bb447881ed1fce5b62a5f8e256
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-06-08 20:38:02 +0400

    feat: implement multi-step registration form with validation

--- src/app/auth/register/register-page.component.ts
+++ src/app/auth/register/register-page.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { Typography } from '../../shared/directive';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    Typography,
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+  nameForm = this.fb.nonNullable.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+  });
+  ageForm = this.fb.group({
+    age: this.fb.control<number | null>(null, [Validators.required, Validators.min(18)]),
+  });
+  emailForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+  passwordForm = this.fb.nonNullable.group({
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+}
