fa3750d945e4b44d941aa01c3f5fad88616c8012
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-06-21 21:03:14 +0400

    feat: implement About Us page with team member cards component(no styling yet)

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Typography } from '../shared/directive';
+import { MemberCardComponent } from './ui/member-card/member-card.component'
+import { TeamMember } from './team-member.model';
+import { TEAM_MEMBERS } from './team-data';
 
 @Component({
   selector: 'app-about-us',
-  imports: [Typography],
+  imports: [Typography, MemberCardComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush //added it just for practise, as part of sprint 3 ( even though it won't be needed in angular 22+)
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  readonly teamMembers: TeamMember[] = TEAM_MEMBERS; // ordinary array, cuz it's static info, no need for signals
+}
--- src/app/about-us/ui/member-card/member-card.component.ts
+++ src/app/about-us/ui/member-card/member-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TeamMember } from '../../team-member.model';
 
 @Component({
   selector: 'app-member-card',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.scss',
 })
-export class MemberCardComponent {}
+export class MemberCardComponent {
+  member = input.required<TeamMember>();// required added ,cuz there is no point in rendering a card without a member(angular will display error if parent forgets to pass it)
+}
