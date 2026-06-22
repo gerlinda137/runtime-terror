21f56367076ad1a4933b25f890d29a828e610240
Radmi1a05 <128687368+radm1la@users.noreply.github.com>
2026-06-22 14:01:39 +0400

    fix:remove publicRoutes and it's checking from auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -3,23 +3,9 @@ import { inject } from '@angular/core';
 import { FULL_ROUTES } from '../../shared/constants';
 import { TokenService } from '../../auth/service/token.service';
 
-export const authGuard: CanActivateFn = (state): boolean | UrlTree => {
+export const authGuard: CanActivateFn = (state,route): boolean | UrlTree => {
   const router = inject(Router);
   const tokenService = inject(TokenService);
-
-  const publicRoutes = [
-    FULL_ROUTES.AUTH_LOGIN,
-    FULL_ROUTES.AUTH_REGISTER,
-    // for test
-    'account',
-    'settings',
-    './',
-    'api/doc'
-  ];
-
-  if (publicRoutes.includes(state.url.toString())) {
-    return true;
-  }
 
   if (tokenService.token()) {
     return true;
