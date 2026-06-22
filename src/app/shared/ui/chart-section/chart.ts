db083e74ca185ae4762772d4b67741e46b7fb859
MaryAnzh <maryang@mail.ru>
2026-06-22 19:47:18 +0300

    feat: add canvas

--- src/app/shared/ui/chart-section/chart.ts
+++ src/app/shared/ui/chart-section/chart.ts
@@ -28,9 +28,40 @@ export class ChartComponent {
     this.api.getKLines(this.symbol(), this.interval())
       .subscribe(data => {
         this.prices = data.map(c => Number(c[4]));
-
-        console.log(this.prices);
+        this.drawChart();
       });
   }
 
+  drawChart() {
+    const canvas = document.getElementById('chartCanvas') as HTMLCanvasElement;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (this.prices.length === 0) return;
+
+    const max = Math.max(...this.prices);
+    const min = Math.min(...this.prices);
+
+    const padding = 20;
+    const width = canvas.width - padding * 2;
+    const height = canvas.height - padding * 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#3f51b5';
+    ctx.lineWidth = 2;
+
+    this.prices.forEach((price, i) => {
+      const x = padding + (i / (this.prices.length - 1)) * width;
+      const y = padding + height - ((price - min) / (max - min)) * height;
+
+      if (i === 0) ctx.moveTo(x, y);
+      else ctx.lineTo(x, y);
+    });
+
+    ctx.stroke();
+  }
 }
