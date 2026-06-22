fd1fdcd4c9e7107a385d2c8069c0a3a38c5a78dc
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-06-22 14:19:18 +0400

    feat: generate contact-us-page component

---
+++ src/app/contact-us/contact-us-page/contact-us-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us-page',
+  imports: [],
+  templateUrl: './contact-us-page.html',
+  styleUrl: './contact-us-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactUsPage {}
