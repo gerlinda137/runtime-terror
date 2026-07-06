a6ecdbe024cf44e7799cffdf5fece8a7617ded87
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-07-06 23:43:00 +0400

    feat: add crypto service wip

---
+++ src/app/core/services/crypto/crypto.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environments';
+import { Observable } from 'rxjs';
+
+export interface OrderPayload {
+  symbol: string;
+  side: 'BUY' | 'SELL';
+  type: 'MARKET' | 'LIMIT';
+  quantity: string;
+  price?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Crypto {
+  private http = inject(HttpClient);
+  private base = environment.apiUrl;
+
+  //account balance
+  // eslint-disable-next-line
+  getAccount(keyId: string): Observable<any> {
+    return this.http.get(`${this.base}/crypto/account`, {
+      params: { keyId },
+    });
+  }
+
+  //current pair price
+  // eslint-disable-next-line
+  getTicker(symbol: string): Observable<any> {
+    return this.http.get(`${this.base}/crypto/ticker`, { params: { symbol } });
+  }
+
+  //create order
+  // eslint-disable-next-line
+  createOrder(keyId: string, payload: OrderPayload): Observable<any> {
+    return this.http.post(`${this.base}/crypto/order`, payload, { params: { keyId } });
+  }
+}
