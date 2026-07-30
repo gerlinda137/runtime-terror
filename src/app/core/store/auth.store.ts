798d26ac3ac6cc6c9fc367c86f062286752dfc45
MaryAnzh <maryang@mail.ru>
2026-07-30 21:38:18 +0300

    fix: fix welcome text

--- src/app/core/store/auth.store.ts
+++ src/app/core/store/auth.store.ts
@@ -9,6 +9,7 @@ import { FULL_ROUTES, USER } from '../../shared/constants';
 import { Token } from '../../auth/service/token';
 import { UserStore } from './user.store';
 import { computed, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 interface AuthState {
   user: User | null;
@@ -34,7 +35,7 @@ export class AuthStore {
   readonly loading$ = this.state$.pipe(map(s => s.loading));
 
   readonly error$ = this.state$.pipe(map(s => s.error));
-  readonly userSig = computed(() => this.state$.value.user);
+  readonly userSig = toSignal(this.user$, { initialValue: null });
   readonly isLoadingSig = computed(() => this.state$.value.loading);
   readonly isAuthenticatedSig = computed(() => Boolean(this.tokenService.token()));
   avatarUrl = signal<string | null>(null);
